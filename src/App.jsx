69be7edd63dfca170ec05f2ad92f71bb9df4e967
lilhalzy <lilhalzy11@gmail.com>
2025-07-29 08:09:59 +0800

    feat: added header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const App = () => {
+  const [score, setScore] = useState(null)
+  const [name, setName] = useState("")
+  
   return (
-    <div>App</div>
+    <div className='max-w-4xl mx-auto p-3 font-serif'> {/* step 3 */}
+      <div className='flex justify-center items-center '>
+        <h1 className='bg-gray-100 px-14 py-2 rounded-4xl text-xl font-sketch font-bold'>Rounding off to Nearest 10</h1>
+      </div>
+      <div className="flex justify-center my-4 mx-auto">
+        <div className="text-sm md:text-lg font-bold">
+          <label htmlFor="name" className="mr-2">Name:</label>
+          <input
+            id="name"
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="border-b-2 text-sm md:text-lg border-black focus:outline-none w-40 md:w-80"
+          />
+        </div>
+        <div className="pl-8 text-sm md:text-lg font-bold">
+          <label htmlFor="score" className="mr-2">Score:</label>
+          <input
+            id="score"
+            type="text"
+            value={score}
+            onChange={(e) => setScore(e.target.value)}
+            className="border-b-2 border-black focus:outline-none w-18 md:w-20"
+          />
+        </div>
+      </div>
+      <div className="flex justify-center items-center">
+        <span>Circle the correct answers.</span>
+      </div>
+    </div>
   )
 }
 
